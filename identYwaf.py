53c547b78aa02948be7eb12d679540455e771f8a
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-01-14 16:22:48 +0100

    Update for Barracuda and Wordfence

--- identYwaf.py
+++ identYwaf.py
@@ -28,7 +28,7 @@ import urllib2
 import zlib
 
 NAME = "identYwaf"
-VERSION = "1.0.5"
+VERSION = "1.0.6"
 BANNER = """
                                    ` __ __ `
  ____  ___      ___  ____   ______ `|  T  T` __    __   ____  _____ 
