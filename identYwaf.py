61e22d0740001324b1ad841fdbce1d8d6ea710c8
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-01-14 16:36:30 +0100

    Minor update

--- identYwaf.py
+++ identYwaf.py
@@ -28,7 +28,7 @@ import urllib2
 import zlib
 
 NAME = "identYwaf"
-VERSION = "1.0.6"
+VERSION = "1.0.7"
 BANNER = """
                                    ` __ __ `
  ____  ___      ___  ____   ______ `|  T  T` __    __   ____  _____ 
