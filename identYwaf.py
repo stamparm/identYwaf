703141968a56fb893c50790dda6ec777e8e5f515
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-01-16 17:01:07 +0100

    Adding new signatures

--- identYwaf.py
+++ identYwaf.py
@@ -29,7 +29,7 @@ import urllib2
 import zlib
 
 NAME = "identYwaf"
-VERSION = "1.0.32"
+VERSION = "1.0.33"
 BANNER = """
                                    ` __ __ `
  ____  ___      ___  ____   ______ `|  T  T` __    __   ____  _____ 
