17ff2e9ad761a44c041226807ef1f4210a3cbaeb
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-01-16 14:33:12 +0100

    Adding support for Imperva SecureSphere

--- identYwaf.py
+++ identYwaf.py
@@ -29,7 +29,7 @@ import urllib2
 import zlib
 
 NAME = "identYwaf"
-VERSION = "1.0.29"
+VERSION = "1.0.30"
 BANNER = """
                                    ` __ __ `
  ____  ___      ___  ____   ______ `|  T  T` __    __   ____  _____ 
