aba3d8e7d9f84c3b001151d71ac602b21652d2e5
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-01-16 01:46:26 +0100

    Updates for bluedon and cddns

--- identYwaf.py
+++ identYwaf.py
@@ -29,7 +29,7 @@ import urllib2
 import zlib
 
 NAME = "identYwaf"
-VERSION = "1.0.26"
+VERSION = "1.0.27"
 BANNER = """
                                    ` __ __ `
  ____  ___      ___  ____   ______ `|  T  T` __    __   ____  _____ 
