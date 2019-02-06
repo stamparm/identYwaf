6f60f6d0fbdc85f33b3ee9562ba1fc5f75d4d2f2
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-02-06 14:35:08 +0100

    Adding entries for alertlogic and profense

--- identYwaf.py
+++ identYwaf.py
@@ -62,7 +62,7 @@ else:
     HTTPCookieProcessor = urllib2.HTTPCookieProcessor
 
 NAME = "identYwaf"
-VERSION = "1.0.72"
+VERSION = "1.0.73"
 BANNER = """
                                    ` __ __ `
  ____  ___      ___  ____   ______ `|  T  T` __    __   ____  _____ 
