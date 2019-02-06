cf6d37d6b7e2aacefa7e43a9a5a2de858eb9d7ec
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-02-06 12:06:31 +0100

    New signature for Kona

--- identYwaf.py
+++ identYwaf.py
@@ -61,7 +61,7 @@ else:
     HTTPCookieProcessor = urllib2.HTTPCookieProcessor
 
 NAME = "identYwaf"
-VERSION = "1.0.68"
+VERSION = "1.0.69"
 BANNER = """
                                    ` __ __ `
  ____  ___      ___  ____   ______ `|  T  T` __    __   ____  _____ 
