a59cc2619c38e725aedb6fced715c34f799c5ee0
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-02-06 14:11:51 +0100

    Update for Approach WAF

--- identYwaf.py
+++ identYwaf.py
@@ -62,7 +62,7 @@ else:
     HTTPCookieProcessor = urllib2.HTTPCookieProcessor
 
 NAME = "identYwaf"
-VERSION = "1.0.71"
+VERSION = "1.0.72"
 BANNER = """
                                    ` __ __ `
  ____  ___      ___  ____   ______ `|  T  T` __    __   ____  _____ 
