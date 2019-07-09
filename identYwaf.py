aff1570a9e403dc91b71821cfdd2638ff1b5ae2d
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-07-09 17:12:49 +0200

    Support for new WAF added (Sniper WAF)

--- identYwaf.py
+++ identYwaf.py
@@ -58,7 +58,7 @@ else:
     HTTPCookieProcessor = urllib2.HTTPCookieProcessor
 
 NAME = "identYwaf"
-VERSION = "1.0.115"
+VERSION = "1.0.116"
 BANNER = r"""
                                    ` __ __ `
  ____  ___      ___  ____   ______ `|  T  T` __    __   ____  _____ 
