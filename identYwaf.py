9bb96277399a801ce77d12eee89ff3a592bae6f2
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-07-09 17:39:53 +0200

    Adding signatures for new WAF (Wapples)

--- identYwaf.py
+++ identYwaf.py
@@ -58,7 +58,7 @@ else:
     HTTPCookieProcessor = urllib2.HTTPCookieProcessor
 
 NAME = "identYwaf"
-VERSION = "1.0.117"
+VERSION = "1.0.118"
 BANNER = r"""
                                    ` __ __ `
  ____  ___      ___  ____   ______ `|  T  T` __    __   ____  _____ 
