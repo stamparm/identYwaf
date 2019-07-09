15b967224ab6130d8a1da009f9f688a5f11f9d2a
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-07-09 16:58:06 +0200

    Adding new signature (WebLand)

--- identYwaf.py
+++ identYwaf.py
@@ -58,7 +58,7 @@ else:
     HTTPCookieProcessor = urllib2.HTTPCookieProcessor
 
 NAME = "identYwaf"
-VERSION = "1.0.114"
+VERSION = "1.0.115"
 BANNER = r"""
                                    ` __ __ `
  ____  ___      ___  ____   ______ `|  T  T` __    __   ____  _____ 
