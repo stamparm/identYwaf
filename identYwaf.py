f34f3c7e9f215cfeb4ea991a8e37921e2d900f36
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-07-16 15:07:15 +0200

    Minor update

--- identYwaf.py
+++ identYwaf.py
@@ -58,7 +58,7 @@ else:
     HTTPCookieProcessor = urllib2.HTTPCookieProcessor
 
 NAME = "identYwaf"
-VERSION = "1.0.118"
+VERSION = "1.0.119"
 BANNER = r"""
                                    ` __ __ `
  ____  ___      ___  ____   ______ `|  T  T` __    __   ____  _____ 
