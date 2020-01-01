11d8a4f653d44b3176b4d1ffbcb61a72f42a3dc8
Miroslav Stampar <miroslav.stampar@gmail.com>
2020-01-01 14:01:42 +0100

    Copyright year bump

--- identYwaf.py
+++ identYwaf.py
@@ -60,7 +60,7 @@ else:
     HTTPCookieProcessor = urllib2.HTTPCookieProcessor
 
 NAME = "identYwaf"
-VERSION = "1.0.124"
+VERSION = "1.0.125"
 BANNER = r"""
                                    ` __ __ `
  ____  ___      ___  ____   ______ `|  T  T` __    __   ____  _____ 
